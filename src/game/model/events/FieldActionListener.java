724c6c34114d0b009185f7d6ba34902158c62ef9
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-31 01:34:29 +0300

    update: class FieldActionListener

--- src/game/model/events/FieldActionListener.java
+++ src/game/model/events/FieldActionListener.java
@@ -7,5 +7,18 @@ import org.jetbrains.annotations.NotNull;
  * Интерфейс слушателя события поля {@link Field}.
  */
 public interface FieldActionListener {
+
+    /**
+     * Ячейка на поле изменила состояние.
+     *
+     * @param event объект события поля.
+     */
     void fieldCellStateChanged(@NotNull FieldActionEvent event);
+
+    /**
+     * На поле была применена блокировка.
+     *
+     * @param event объект события поля.
+     */
+    void fieldObstructionExecuted(@NotNull FieldActionEvent event);
 }
