916e7f221b60401ee4ad65c5132e7e18a7b28104
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-31 01:53:09 +0300

    feat: class ImageUtils

---
+++ src/game/ui/utils/ImageUtils.java
@@ -0,0 +1,20 @@
+package game.ui.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageUtils {
+    public static BufferedImage resizeImage(BufferedImage img, int width, int height) {
+        Image tmpImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g = bufferedImage.createGraphics();
+        g.drawImage(tmpImg, 0, 0, null);
+        g.dispose();
+
+        return bufferedImage;
+    }
+
+    public static final String IMAGE_PATH = "src/main/resources/";
+    public static final Color BACKGROUND_COLOR = Color.decode("#F5F5DC"); // Soft beige
+}
