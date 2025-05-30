4517f1af8b41b79024ec57bd065bc80ba7c4d49c
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-31 01:05:27 +0300

    feat: class PermanentOneCellObstruction

---
+++ src/game/model/field/obstructions/PermanentOneCellObstruction.java
@@ -0,0 +1,12 @@
+package game.model.field.obstructions;
+
+import game.model.field.AbstractObstruction;
+import game.model.field.Cell;
+
+public class PermanentOneCellObstruction extends AbstractObstruction {
+
+    @Override
+    public boolean execute(Cell target) {
+        return super.block(target);
+    }
+}
