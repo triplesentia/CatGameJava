e2e5df53d39db174ce1d0a98aed1220a9d826e67
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-31 01:34:20 +0300

    update: class FieldActionEvent

--- src/game/model/events/FieldActionEvent.java
+++ src/game/model/events/FieldActionEvent.java
@@ -8,10 +8,72 @@ import java.util.EventObject;
  * Объект события класса поле {@link Field}.
  */
 public class FieldActionEvent extends EventObject {
+
+    //region КОНСТРУКТОР
+
+    /**
+     * Создаёт событие.
+     *
+     * @param source объект, на котором изначально произошло событие
+     * @throws IllegalArgumentException если source равен null
+     */
     public FieldActionEvent(Object source) {
         super(source);
     }
+
+    //endregion
+
+    //region ЯЧЕЙКА
+
+    /**
+     * Ячейка, связанная с событием поля.
+     */
     private Cell cell;
-    public void setCell(Cell cell) { this.cell = cell; }
-    public Cell getCell() { return cell; }
+
+    /**
+     * Установить ячейку {@link FieldActionEvent#cell}.
+     *
+     * @param cell ячейка.
+     */
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    /**
+     * Получить ячейку {@link FieldActionEvent#cell}.
+     *
+     * @return ячейка.
+     */
+    public Cell getCell() {
+        return cell;
+    }
+
+    //endregion
+
+    //region ТИП БЛОКИРОВКИ
+
+    /**
+     * Тип блокировки, связанный с событием.
+     */
+    private Cell.ObstructionType obstructionType;
+
+    /**
+     * Установить тип блокировки {@link FieldActionEvent#obstructionType}.
+     *
+     * @param obstructionType тип блокировки.
+     */
+    public void setObstructionType(Cell.ObstructionType obstructionType) {
+        this.obstructionType = obstructionType;
+    }
+
+    /**
+     * Получить тип блокировки {@link FieldActionEvent#obstructionType}.
+     *
+     * @return тип блокировки.
+     */
+    public Cell.ObstructionType getObstructionType() {
+        return obstructionType;
+    }
+
+    //endregion
 }
