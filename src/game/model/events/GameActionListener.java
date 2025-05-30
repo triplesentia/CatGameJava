a7000600e87b301a2e6511ea3a3c5a64fd9d2088
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-31 00:45:16 +0300

    feat: interface GameActionListener

---
+++ src/game/model/events/GameActionListener.java
@@ -0,0 +1,23 @@
+package game.model.events;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Интерфейс слушателя события класса игры {@link game.model.Game}.
+ */
+public interface GameActionListener extends java.util.EventListener {
+
+    /**
+     * Кот переместился.
+     *
+     * @param event объект события класса игры.
+     */
+    void catIsMoved(@NotNull GameActionEvent event);
+
+    /**
+     * Статус игры изменился.
+     *
+     * @param event объект события класса игры.
+     */
+    void gameStatusChanged(@NotNull GameActionEvent event);
+}
