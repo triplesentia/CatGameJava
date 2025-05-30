ca2e0424112be0848a7e3680fbc4859ee6572397
Elizaveta Lim <eliza.volga21@gmail.com>
2025-05-30 23:59:50 +0300

    feat: interface CellActionListener

---
+++ src/game/model/events/CellActionListener.java
@@ -0,0 +1,19 @@
+package game.model.events;
+
+import org.jetbrains.annotations.NotNull;
+import game.model.field.Cell;
+
+import java.util.EventListener;
+
+/**
+ * Интерфейс слушателя события класса ячейка {@link Cell}.
+ */
+public interface CellActionListener extends EventListener {
+
+    /**
+     * Ячейка изменила состояние.
+     *
+     * @param event объект события класса ячейка.
+     */
+    void cellStateChanged(@NotNull CellActionEvent event);
+}
