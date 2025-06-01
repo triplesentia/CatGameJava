9c84ab5e6a6d088b34e04f7a8e51e6bc105df63d
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-01 03:15:58 +0300

    fix: obstruction panel reset

--- src/game/Main.java
+++ src/game/Main.java
@@ -60,6 +60,8 @@ public class Main {
             content.removeAll();
             FieldWidget fieldWidget = new FieldWidget(game.getGameField(), widgetFactory);
             content.add(fieldWidget);
+            obstructionPanel = null;
+            obstructionButtons.clear();
             createOrUpdateObstructionPanel();
 
             pack();
